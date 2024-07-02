b2b8d8c7689e1aed1a28d21673d48819bb964ca8
Dass33 <danieldvoraku@gmail.com>
2024-07-02 08:03:39 +0200

    win check added, winner in red

--- dist/maint.js
+++ dist/maint.js
@@ -23,7 +23,8 @@ class PlayerTurns {
         }
         this.turnOf++;
     }
-    winRowChech(arr, rowNum) {
+    winRowChech(arr, rowNum, rowImageArr) {
+        const row = 0;
         let winnArr;
         let rowColumn;
         this.symbolStreak = 1;
@@ -39,13 +40,14 @@ class PlayerTurns {
             }
             if (this.symbolStreak >= this.streakToWin) {
                 winnArr.unshift([i - (this.symbolStreak - 1), rowNum]);
-                this.endGame(winnArr, this.lastElement, arr);
+                this.endGame(winnArr, rowImageArr, this.lastElement);
             }
             this.lastElement = inRowItem;
         });
         this.lastElement = "";
     }
-    winColumnChech(arr, columnNum) {
+    winColumnChech(arr, columnNum, rowImageArr) {
+        const column = 1;
         let winnArr = [];
         let rowColumn;
         this.symbolStreak = 1;
@@ -62,13 +64,14 @@ class PlayerTurns {
             }
             if (this.symbolStreak >= this.streakToWin) {
                 winnArr.unshift([columnNum, i - (this.symbolStreak - 1)]);
-                this.endGame(winnArr, this.lastElement, arr);
+                this.endGame(winnArr, rowImageArr, this.lastElement);
             }
             this.lastElement = inColumnItem;
         });
         this.lastElement = "";
     }
-    winDiagonalRightCheck(arr, columnNum, rowNum) {
+    winDiagonalRightCheck(arr, columnNum, rowNum, rowImageArr) {
+        const diagonalRight = 2;
         let winnArr = [];
         let rowColumn;
         this.symbolStreak = 1;
@@ -79,7 +82,7 @@ class PlayerTurns {
                 const inDiagonalItem = arr[currRow][currColumn];
                 if (this.lastElement == inDiagonalItem && this.lastElement !== undefined) {
                     this.symbolStreak++;
-                    rowColumn = [currRow, currColumn];
+                    rowColumn = [currColumn, currRow];
                     winnArr.push(rowColumn);
                 }
                 else {
@@ -87,15 +90,16 @@ class PlayerTurns {
                     winnArr = [];
                 }
                 if (this.symbolStreak >= this.streakToWin) {
-                    winnArr.unshift([columnNum, i - (this.symbolStreak - 1)]);
-                    this.endGame(winnArr, this.lastElement, arr);
+                    winnArr.unshift([i - (this.symbolStreak - 1), rowNum + i - columnNum - (this.symbolStreak - 1)]);
+                    this.endGame(winnArr, rowImageArr, this.lastElement);
                 }
                 this.lastElement = inDiagonalItem;
             }
         }
         this.lastElement = "";
     }
-    winDiagonalLeftCheck(arr, columnNum, rowNum) {
+    winDiagonalLeftCheck(arr, columnNum, rowNum, rowImageArr) {
+        const diagonalLeft = 3;
         let winnArr = [];
         let rowColumn;
         this.symbolStreak = 1;
@@ -106,7 +110,7 @@ class PlayerTurns {
                 const inDiagonalItem = arr[currRow][currColumn];
                 if (this.lastElement == inDiagonalItem && this.lastElement !== undefined) {
                     this.symbolStreak++;
-                    rowColumn = [currRow, currColumn];
+                    rowColumn = [currColumn, currRow];
                     winnArr.push(rowColumn);
                 }
                 else {
@@ -114,20 +118,19 @@ class PlayerTurns {
                     winnArr = [];
                 }
                 if (this.symbolStreak >= this.streakToWin) {
-                    winnArr.unshift([columnNum, i - (this.symbolStreak - 1)]);
-                    this.endGame(winnArr, this.lastElement, arr);
+                    winnArr.unshift([(this.boardSize - i + this.symbolStreak - 1), (columnNum + rowNum + i) - this.boardSize - (this.symbolStreak - 1)]);
+                    this.endGame(winnArr, rowImageArr, this.lastElement);
                 }
                 this.lastElement = inDiagonalItem;
             }
         }
         this.lastElement = "";
     }
-    endGame(winArr, winner, grid) {
-        alert("Winer is: " + winner);
+    endGame(winArr, grid, winner) {
+        // alert("Winer is: " + winner);
         this.gameIsGoing = false;
-        //finish here//
-        winArr.forEach(([row, column]) => {
-            grid[row][column];
+        winArr.forEach(([column, row]) => {
+            grid[row][column].src = `./img/${winner}Win.svg`;
         });
     }
 }
@@ -136,11 +139,13 @@ const size = 8;
 const symbolsToWin = 4;
 const playerTurn = new PlayerTurns(symbolsToWin, size);
 const gridArr = [];
+const imageArr = [];
 let rowNum = 0;
 for (let i = 0; i < size; i++) {
     const tr = document.createElement("tr");
     tr.classList.add("boardTr");
     const rowArr = [];
+    const rowImageArr = [];
     for (let columnNum = 0; columnNum < size; columnNum++) {
         const td = document.createElement("td");
         td.classList.add("boardTd");
@@ -152,11 +157,12 @@ for (let i = 0; i < size; i++) {
         ((currentRowNum) => {
             td.addEventListener("click", () => {
                 playerTurn.placeOX(img);
-                rowArr[columnNum] = (img.getAttribute("symbol"));
-                playerTurn.winRowChech(gridArr, currentRowNum);
-                playerTurn.winColumnChech(gridArr, columnNum);
-                playerTurn.winDiagonalRightCheck(gridArr, columnNum, currentRowNum);
-                playerTurn.winDiagonalLeftCheck(gridArr, columnNum, currentRowNum);
+                rowArr[columnNum] = img.getAttribute("symbol");
+                rowImageArr[columnNum] = img;
+                playerTurn.winRowChech(gridArr, currentRowNum, imageArr);
+                playerTurn.winColumnChech(gridArr, columnNum, imageArr);
+                playerTurn.winDiagonalRightCheck(gridArr, columnNum, currentRowNum, imageArr);
+                playerTurn.winDiagonalLeftCheck(gridArr, columnNum, currentRowNum, imageArr);
             });
         })(rowNum);
         img.classList.add("boardImg");
@@ -164,6 +170,7 @@ for (let i = 0; i < size; i++) {
         tr.appendChild(td);
     }
     gridArr.push(rowArr);
+    imageArr.push(rowImageArr);
     board === null || board === void 0 ? void 0 : board.appendChild(tr);
     rowNum++;
 }
